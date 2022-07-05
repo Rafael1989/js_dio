806619c9d65c8fdcded65eb7ee1e94e0ae7791ec
Rafael <flns_rafa@hotmail.com>
2022-07-05 14:08:34 +0200

    string

---
+++ atribuindo-valores/script.js
@@ -0,0 +1,48 @@
+numerOne = 1;
+
+console.log(numerOne + 2);
+
+
+var firstName = 'Joao';
+var lastName = "Souza";
+
+if(firstName === "Joao"){
+    var firstName = "Pedro";
+    let lastName = "Silva";
+}
+
+console.log(firstName, lastName);
+
+var firstName = 'Joao';
+let lastName = "Souza";
+
+if(firstName === "Joao"){
+    let lastName = "Rodrigues";
+    var firstName = "Pedro";
+    lastName = "Silva";
+
+    console.log(firstName, lastName);
+}
+
+console.log(firstName, lastName);
+nome
+'Joao'
+nome[1]
+'o'
+nome.length
+4
+concatenado
+concatenado = nome + " " + sobrenome
+concatenado = nome + "\n" + sobrenome
+concatenado = nome + "\n\n" + sobrenome
+concatenado = nome + '\n\n' + sobrenome
+concatenado = `${nome} ${sobrenome}`
+concatenado = `${nome} ${sobrenome}`
+concatenado = "\""
+let frase = "Ola, tudo bem?"
+frase.split(" ")
+frase.includes("tudo")
+frase.startsWith("O")
+frase.endsWith("?")
+frase.replace(",","!")
+let stringModificada = frase.replace(",","!")
