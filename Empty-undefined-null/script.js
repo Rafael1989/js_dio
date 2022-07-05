ae55c66fedc92c0f5ed1ebde2119d9292ea8ceaf
Rafael <flns_rafa@hotmail.com>
2022-07-05 14:53:19 +0200

    Empty Undefined Null

---
+++ Empty-undefined-null/script.js
@@ -0,0 +1,19 @@
+abacaxi
+
+typeof abacaxi // undefined
+
+let mamao 
+
+typeof mamao // undefined
+
+!mamao // true (oposto de undefined é true)
+
+let uva = ""
+
+typeof uva // string (Empty or "")
+
+let pera = null
+
+typeof pera // object (null)
+
+!pera // true (oposto de null é true)
