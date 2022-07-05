d3c650ffd9a9ce8a1b2a9228dbadce30598646e2
Rafael <flns_rafa@hotmail.com>
2022-07-05 14:09:19 +0200

    string

--- atribuindo-valores/script.js
+++ atribuindo-valores/script.js
@@ -25,24 +25,3 @@ if(firstName === "Joao"){
 }
 
 console.log(firstName, lastName);
-nome
-'Joao'
-nome[1]
-'o'
-nome.length
-4
-concatenado
-concatenado = nome + " " + sobrenome
-concatenado = nome + "\n" + sobrenome
-concatenado = nome + "\n\n" + sobrenome
-concatenado = nome + '\n\n' + sobrenome
-concatenado = `${nome} ${sobrenome}`
-concatenado = `${nome} ${sobrenome}`
-concatenado = "\""
-let frase = "Ola, tudo bem?"
-frase.split(" ")
-frase.includes("tudo")
-frase.startsWith("O")
-frase.endsWith("?")
-frase.replace(",","!")
-let stringModificada = frase.replace(",","!")
---
+++ string/script.js
@@ -0,0 +1,18 @@
+nome
+nome[1]
+nome.length
+concatenado
+concatenado = nome + " " + sobrenome
+concatenado = nome + "\n" + sobrenome
+concatenado = nome + "\n\n" + sobrenome
+concatenado = nome + '\n\n' + sobrenome
+concatenado = `${nome} ${sobrenome}`
+concatenado = `${nome} ${sobrenome}`
+concatenado = "\""
+let frase = "Ola, tudo bem?"
+frase.split(" ")
+frase.includes("tudo")
+frase.startsWith("O")
+frase.endsWith("?")
+frase.replace(",","!")
+let stringModificada = frase.replace(",","!")
