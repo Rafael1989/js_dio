9fb667fb965f96b914bfd2ed201eaba6d8d17a01
Rafael <flns_rafa@hotmail.com>
2022-07-05 14:38:30 +0200

    Objetos

---
+++ Objetos/script.js
@@ -0,0 +1,25 @@
+let obj = {}
+
+typeof obj
+
+obj.name = "julia"
+
+obj.age = 20
+
+Object.values(obj) // valores do objeto (julia, 20)
+
+Object.keys(obj) // As chaves (name, age)
+
+let person = {name: "Julia", age: 20, adress: "Rua 2"}
+
+person.name 
+
+person["name"]
+
+person["numberOfSiblings"] = 3
+
+let mom = "nameOfMom"
+
+person[mom] = "Maria"
+
+person.mom = "marta"
