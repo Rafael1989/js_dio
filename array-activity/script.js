bd16a4e22ade7251a9b56479d8d0d28232fc7ec4
Rafael <flns_rafa@hotmail.com>
2022-07-07 16:51:54 +0200

    AP map, reduce, filter

--- array-activity/script.js
+++ array-activity/script.js
@@ -876,6 +876,91 @@ const saldoFinal = listPrecos.reduce(function(saldoDisponivel, preco){
 
 console.log(saldoFinal);
 
+const maca = {
+    value: 2
+}
+
+const laranja = {
+    value: 3
+}
+
+function mapComThis(arr, thisArg){
+    return arr.map(function(item){
+        return item * this.value;
+    }, thisArg);
+}
+
+const nums2 = [1,2];
+
+console.log('this -> maca', mapComThis(nums2, maca));
+
+console.log('this -> laranja', mapComThis(nums2, laranja));
+
+function mapSemThis(arr){
+    return arr.map(function(item){
+        return item * 2;
+    });
+}
+
+const nums3 = [2,4,6,8,10];
+
+console.log(mapSemThis(nums3));
+
+
+function filtraPares(arr){
+    return arr.filter(callback)
+}
+
+function callback(item){
+    return item % 2 === 0;
+}
+
+const meuArray = [1,23,55,67,30,2,4]
+
+console.log(filtraPares(meuArray));
+
+function somaNumeros(arr){
+    return arr.reduce(function(prev, current){
+        console.log({prev})
+        console.log({current})
+        return prev + current;
+    })
+}
+
+const arr = [1,2];
+
+console.log(somaNumeros(arr));
+
+const lista = [
+    {
+        name: 'sabao em po',
+        preco: 30
+    },
+    {
+        name: 'cereal',
+        preco: 12
+    },
+    {
+        name: 'toalha',
+        preco: 30
+    }
+];
+
+const saldoDisponivel = 100;
+
+function calculaSaldo(saldoDisponivel, lista){
+    return lista.reduce(function(prev, current, index){
+        console.log("rodada ", index + 1)
+        console.log({prev})
+        console.log({current})
+        return prev - current.preco;
+    }, saldoDisponivel);
+}
+
+console.log(calculaSaldo(saldoDisponivel, lista))
+
+
+
 
 
 
