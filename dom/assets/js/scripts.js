f722f5a2ad41a2f353143bfc3904ff1e342d89ce
Rafael <flns_rafa@hotmail.com>
2022-07-12 11:11:15 +0200

    JS DOM

--- dom/assets/js/scripts.js
+++ dom/assets/js/scripts.js
@@ -1,7 +1,8 @@
 const h1 = document.getElementById("page-title");
 const button = document.getElementById("mode-selector");
-const footer = document.getElementById("footerId");
-const body = document.getElementById("bodyId");
+const footer = document.getElementsByTagName("footer")[0];
+const body = document.getElementsByTagName("body")[0];
+const darkModeClass = "dark-mode";
 
 
 button.addEventListener("click", toogleDarkMode);
@@ -11,4 +12,18 @@ function toogleDarkMode(){
     button.classList.toggle("dark-mode");
     footer.classList.toggle("dark-mode");
     body.classList.toggle("dark-mode");
+    changeText();
+}
+
+function changeText(){
+    const lightMode = "Light Mode";
+    const darkMode = "Dark Mode";
+    if(body.classList.contains(darkModeClass)){
+        button.innerHTML = "Light Mode";
+        h1.innerHTML = darkMode + " ON";
+        return;
+    }
+
+    button.innerHTML = darkMode;
+    h1.innerHTML = lightMode + " ON";
 }
